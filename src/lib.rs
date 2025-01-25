57b80784c28ae0113d08282bbc1dcc69fc64b90f
InimaJin <>
2025-01-25 01:31:26 +0100

    Implemented algorithm for row echelon form.

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,15 @@ use std::{
     default::Default,
     error::Error,
     fmt,
-    ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign},
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign},
 };
 
 pub struct Matrix<T> {
-    content: Vec<Vec<T>>,
+    grid: Vec<Vec<T>>,
 }
 
-//Types implementing this trait can be used to build matrices
+/* Types implementing this trait can be used to build matrices.
+ * T::default() should be 0. */
 trait MatrixElement<T>:
     Copy
     + Clone
@@ -23,6 +24,8 @@ trait MatrixElement<T>:
     + SubAssign
     + Mul<Output = T>
     + MulAssign
+    + Div<Output = T>
+    + DivAssign
 {
 }
 impl<T> MatrixElement<T> for T where
@@ -37,6 +40,8 @@ impl<T> MatrixElement<T> for T where
         + SubAssign
         + Mul<Output = T>
         + MulAssign
+        + Div<Output = T>
+        + DivAssign
 {
 }
 
@@ -47,7 +52,7 @@ impl<T: MatrixElement<T>> Matrix<T> {
             return Err(Box::from(Self::DIMENSION_ERR));
         }
         let vec = vec![vec![init; width]; height];
-        Ok(Self { content: vec })
+        Ok(Self { grid: vec })
     }
 
     pub fn from_vec(vec: Vec<Vec<T>>) -> Result<Self, Box<dyn Error>> {
@@ -62,26 +67,80 @@ impl<T: MatrixElement<T>> Matrix<T> {
             }
         }
 
-        Ok(Self { content: vec })
+        Ok(Self { grid: vec })
     }
 
     /* Creates and returns a scalar multiple 'lambda' of the identity matrix */
     pub fn scalar_matrix(lambda: T, width: usize) -> Result<Self, Box<dyn Error>> {
         let mut matrix = Self::build(T::default(), width, width)?;
         for i in 0..width {
-            matrix.content[i][i] = lambda;
+            matrix.grid[i][i] = lambda;
         }
         Ok(matrix)
     }
 
     pub fn width(&self) -> usize {
-        self.content[0].len()
+        self.grid[0].len()
     }
     pub fn height(&self) -> usize {
-        self.content.len()
+        self.grid.len()
     }
     pub fn size(&self) -> usize {
         self.width() * self.height()
+    }
+
+    fn swap_rows(&mut self, row1: usize, row2: usize) {
+        if row1 < self.grid.len() && row2 < self.grid.len() {
+            self.grid.swap(row1, row2);
+        }
+    }
+
+    /* Transforms the matrix into row echelon form and returns the number of rows swapped in the
+     * process. */
+    pub fn to_row_echelon(&mut self) -> i32 {
+        let mut rows_swapped = 0;
+        for pivot_row in 0..self.height() - 1 {
+            if pivot_row == self.width() {
+                //Matrix has more rows than columns.
+                return rows_swapped;
+            }
+
+            let mut pivot = self.grid[pivot_row][pivot_row];
+            //Swap rows in case pivot == 0
+            if pivot == T::default() {
+                let mut swapped = false;
+                for row in pivot_row + 1..self.height() {
+                    if self.grid[row][pivot_row] != pivot {
+                        self.swap_rows(pivot_row, row);
+                        rows_swapped += 1;
+                        swapped = true;
+
+                        pivot = self.grid[pivot_row][pivot_row];
+                        break;
+                    }
+                }
+                //If all elements in current column below pivot element are 0 as well.
+                if !swapped {
+                    continue;
+                }
+            }
+            for row in pivot_row + 1..self.height() {
+                if self.grid[row][pivot_row] == T::default() {
+                    continue;
+                }
+
+                /* fac is the factor by which the pivot would be multiplied in order for the
+                 * elements below the pivot to become 0 when subtracting the product of pivot and
+                 * fac. */
+                let fac = self.grid[row][pivot_row] / pivot;
+                for i in pivot_row..self.width() {
+                    let subtract = fac * self.grid[pivot_row][i];
+                    self.grid[row][i] -= subtract;
+                }
+            }
+        }
+
+        rows_swapped
     }
 
     /* Computes and returns the determinant of a square matrix. Currently only for 2x2 and 3x3
@@ -91,29 +150,30 @@ impl<T: MatrixElement<T>> Matrix<T> {
             return None;
         }
         return Some(match self.width() {
-            1 => self.content[0][0],
+            1 => self.grid[0][0],
             2 => self.det_2x2(),
             3 => self.det_3x3(),
             _ => T::default(), // TODO: NxN matrices
         });
     }
     fn det_2x2(&self) -> T {
-        return self.content[0][0] * self.content[1][1] - self.content[1][0] * self.content[0][1];
+        return self.grid[0][0] * self.grid[1][1] - self.grid[1][0] * self.grid[0][1];
     }
+
     /* Computation using rule of Sarrus */
     fn det_3x3(&self) -> T {
         let mut res = T::default();
-        //top holds the values of the first row within each iteration.
-        //It serves as the entry point for each diagonal multiplication.
-        //add dictates whether tmp_prod should be added or subtracted to/from res.
+        /* top holds the values of the first row within each iteration.
+         * It serves as the entry point for each diagonal multiplication.
+         * add dictates whether tmp_prod should be added or subtracted to/from res. */
         let (mut top, mut add) = (0, true);
         while top >= 0 {
             let mut col = top as usize;
             //Temporary product of values in a diagonal
-            let mut tmp_prod = self.content[0][top as usize];
+            let mut tmp_prod = self.grid[0][top as usize];
             for row in 0..self.height() {
                 if row != 0 {
-                    tmp_prod *= self.content[row][col];
+                    tmp_prod *= self.grid[row][col];
                 }
                 if add {
                     col = (col + 1) % self.width();
@@ -141,11 +201,11 @@ impl<T: MatrixElement<T>> Matrix<T> {
 impl<T: fmt::Display> fmt::Display for Matrix<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut output = String::new();
-        let (width, height) = (self.content[0].len(), self.content.len());
+        let (width, height) = (self.grid[0].len(), self.grid.len());
         for i in 0..height {
             output.push_str("[");
             for j in 0..width {
-                let s = format!(" {} ", self.content[i][j]);
+                let s = format!(" {} ", self.grid[i][j]);
                 output.push_str(&s);
             }
             output.push_str("]");
@@ -164,7 +224,7 @@ impl<T: MatrixElement<T>> Mul<T> for Matrix<T> {
     fn mul(mut self, rhs: T) -> Self::Output {
         for i in 0..self.height() {
             for j in 0..self.width() {
-                self.content[i][j] *= rhs;
+                self.grid[i][j] *= rhs;
             }
         }
         self
@@ -174,7 +234,7 @@ impl<T: MatrixElement<T>> MulAssign<T> for Matrix<T> {
     fn mul_assign(&mut self, rhs: T) {
         for i in 0..self.height() {
             for j in 0..self.width() {
-                self.content[i][j] *= rhs;
+                self.grid[i][j] *= rhs;
             }
         }
     }
@@ -190,7 +250,7 @@ impl<T: MatrixElement<T>> Add<Matrix<T>> for Matrix<T> {
 
         for i in 0..self.height() {
             for j in 0..self.width() {
-                self.content[i][j] += rhs.content[i][j];
+                self.grid[i][j] += rhs.grid[i][j];
             }
         }
         self
@@ -204,7 +264,7 @@ impl<T: MatrixElement<T>> AddAssign<Matrix<T>> for Matrix<T> {
 
         for i in 0..self.height() {
             for j in 0..self.width() {
-                self.content[i][j] += rhs.content[i][j];
+                self.grid[i][j] += rhs.grid[i][j];
             }
         }
     }
@@ -217,7 +277,7 @@ impl<T: MatrixElement<T>> Sub<Matrix<T>> for Matrix<T> {
         }
         for i in 0..self.height() {
             for j in 0..self.width() {
-                self.content[i][j] -= rhs.content[i][j];
+                self.grid[i][j] -= rhs.grid[i][j];
             }
         }
         self
@@ -227,7 +287,7 @@ impl<T: MatrixElement<T>> SubAssign<Matrix<T>> for Matrix<T> {
     fn sub_assign(&mut self, rhs: Matrix<T>) {
         for i in 0..self.height() {
             for j in 0..self.width() {
-                self.content[i][j] -= rhs.content[i][j];
+                self.grid[i][j] -= rhs.grid[i][j];
             }
         }
     }
@@ -247,8 +307,7 @@ impl<T: MatrixElement<T>> Mul<Matrix<T>> for Matrix<T> {
         for row in 0..self.height() {
             for col in 0..rhs.width() {
                 for pointer in 0..self.width() {
-                    product.content[row][col] +=
-                        self.content[row][pointer] * rhs.content[pointer][col];
+                    product.grid[row][col] += self.grid[row][pointer] * rhs.grid[pointer][col];
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,4 @@
 use matrix::Matrix;
 
 fn main() {
-    let m1 = Matrix::from_vec(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]]).unwrap();
-    let m2 = Matrix::from_vec(vec![vec![3, 2, 1], vec![4, 3, 7], vec![5, 6, -4]]).unwrap();
-
-    let m1_m2 = m2 - m1;
-    println!("{m1_m2}");
-
-    let m3 = Matrix::from_vec(vec![
-        vec![-8.0, 3.0, 29.0],
-        vec![300.0, 92.0, 7.4],
-        vec![3.0, -145.0, 0.0],
-    ])
-    .unwrap();
-    println!("{}", m3.det().unwrap());
 }
